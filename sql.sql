824887ed013c87678fd7276c2d597f651ca6b64f
Eduardo Freitas <73096891+Sync-BR@users.noreply.github.com>
2024-10-15 14:51:45 -0300

    update sql

--- sql.sql
+++ sql.sql
@@ -1,22 +1,25 @@
 CREATE SCHEMA `petrg`;
 
-CREATE TABLE `petrg`.`logins` (
-                                  `id` INT AUTO_INCREMENT PRIMARY KEY,
-                                  `username` VARCHAR(255) NOT NULL,
-                                  `password` VARCHAR(255) NOT NULL
+CREATE TABLE `petrg`.`logins`
+(
+    `id`       INT AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    UNIQUE (`usernamef`),
 );
 
-CREATE TABLE `petrg`.`usuarios` (
-                                    `id` INT NOT NULL AUTO_INCREMENT,
-                                    `name` VARCHAR(60) NOT NULL,
-                                    `surname` VARCHAR(20),
-                                    `cpf` VARCHAR(15) NOT NULL,
-                                    `telephone` VARCHAR(14) NOT NULL,
-                                    `email` VARCHAR(255) NOT NULL,
-                                    `date` DATE NOT NULL,
-                                    `login_id` INT,
-                                    PRIMARY KEY (`id`),
-                                    UNIQUE (`cpf`),
-                                    UNIQUE (`email`),
-                                    CONSTRAINT fk_login FOREIGN KEY (`login_id`) REFERENCES `petrg`.`logins`(`id`)
+CREATE TABLE `petrg`.`usuarios`
+(
+    `id`        INT          NOT NULL AUTO_INCREMENT,
+    `name`      VARCHAR(60)  NOT NULL,
+    `surname`   VARCHAR(20),
+    `cpf`       VARCHAR(15)  NOT NULL,
+    `telephone` VARCHAR(14)  NOT NULL,
+    `email`     VARCHAR(255) NOT NULL,
+    `date`      DATE         NOT NULL,
+    `login_id`  INT,
+    PRIMARY KEY (`id`),
+    UNIQUE (`cpf`),
+    UNIQUE (`email`),
+    CONSTRAINT fk_login FOREIGN KEY (`login_id`) REFERENCES `petrg`.`logins` (`id`)
 );

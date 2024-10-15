478e69ca4f575e11ebf91064634d25f39164ba07
Eduardo Freitas <73096891+Sync-BR@users.noreply.github.com>
2024-10-15 14:52:08 -0300

    Add autenticação de usuário

---
+++ src/main/java/com/meuspets/petlove/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.meuspets.petlove.controller;
+
+import com.meuspets.petlove.handling.UsuarioHanding;
+import com.meuspets.petlove.model.LoginModel;
+import com.meuspets.petlove.repository.LoginRepository;
+import com.meuspets.petlove.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/login")
+public class LoginController extends UserService {
+    @Autowired
+    private LoginRepository loginRepository;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<HttpStatus> login(@RequestBody LoginModel login) throws UsuarioHanding {
+        if (!check(login)) {
+            LoginModel loginFound = loginRepository.findByUsername(login.getUsername());
+            if(loginFound.getPassword().equals(login.getPassword())) {
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+}

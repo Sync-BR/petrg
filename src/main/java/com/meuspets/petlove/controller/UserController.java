640b582af4e4356ce977a75c46f9efd27d4d61bb
Eduardo Freitas <73096891+Sync-BR@users.noreply.github.com>
2024-10-15 15:03:49 -0300

    Melhorado estrutura

--- src/main/java/com/meuspets/petlove/controller/UserController.java
+++ src/main/java/com/meuspets/petlove/controller/UserController.java
@@ -17,9 +17,10 @@
         private UsuarioRepository usuarioRepository;
 
         //Create user api
-        @PostMapping("/create/user{login}/pass{password}")
-        public ResponseEntity<HttpStatus> registerUser(@RequestBody UsuarioModel user, @PathVariable String login, @PathVariable String password) throws UsuarioHanding {
-            LoginModel userLogin = new LoginModel(login, password);
+        @PostMapping("/create/user")
+        public ResponseEntity<HttpStatus> registerUser(@RequestBody UsuarioModel user) throws UsuarioHanding {
+            LoginModel userLogin = new LoginModel(user.getLogin().getUsername(),user.getLogin().getPassword());
+            System.out.println(userLogin);
             if (check(userLogin)) {
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             } else if (!check(userLogin)) {

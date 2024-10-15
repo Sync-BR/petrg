e11616a421fbec2bfdb6117d836aa44615193039
Eduardo Freitas <73096891+Sync-BR@users.noreply.github.com>
2024-10-15 13:55:30 -0300

    Adicionado api para criação de novas contas

--- src/main/java/com/meuspets/petlove/controller/UserController.java
+++ src/main/java/com/meuspets/petlove/controller/UserController.java
@@ -1,28 +1,34 @@
-package com.meuspets.petlove.controller;
+    package com.meuspets.petlove.controller;
 
-import com.meuspets.petlove.handling.UsuarioHanding;
-import com.meuspets.petlove.model.UsuarioModel;
-import com.meuspets.petlove.repository.UsuarioRepository;
-import com.meuspets.petlove.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+    import com.meuspets.petlove.handling.UsuarioHanding;
+    import com.meuspets.petlove.model.LoginModel;
+    import com.meuspets.petlove.model.UsuarioModel;
+    import com.meuspets.petlove.repository.UsuarioRepository;
+    import com.meuspets.petlove.service.UserService;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/api/user")
-public class UserController extends UserService {
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+    @RestController
+    @RequestMapping("/api/user")
+    public class UserController extends UserService {
+        @Autowired
+        private UsuarioRepository usuarioRepository;
 
-    @PostMapping("/create")
-    public ResponseEntity<HttpStatus> registerUser(@RequestBody UsuarioModel user) throws UsuarioHanding {
-        if(!checkNullUser(user)){
-           return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        //Create user api
+        @PostMapping("/create/user{login}/pass{password}")
+        public ResponseEntity<HttpStatus> registerUser(@RequestBody UsuarioModel user, @PathVariable String login, @PathVariable String password) throws UsuarioHanding {
+            System.out.println(user.getDatOfBirth());
+            LoginModel userLogin = new LoginModel(login, password);
+            if (check(userLogin)) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            } else if (!check(userLogin)) {
+                user.setLogin(userLogin);
+                usuarioRepository.save(user);
+                return new ResponseEntity<>(HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
         }
-        return new ResponseEntity<>(HttpStatus.CREATED);
     }
-}

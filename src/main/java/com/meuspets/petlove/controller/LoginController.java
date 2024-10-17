1a3f0c7fc71003d1fc8eb116e25077227dd0f97b
Eduardo Freitas <73096891+Sync-BR@users.noreply.github.com>
2024-10-16 22:35:01 -0300

    Add @CrossOrigin

--- src/main/java/com/meuspets/petlove/controller/LoginController.java
+++ src/main/java/com/meuspets/petlove/controller/LoginController.java
@@ -15,12 +15,13 @@ public class LoginController extends UserService {
     @Autowired
     private LoginRepository loginRepository;
 
+    @CrossOrigin(origins = "177.208.2.93:4200")
     @PostMapping("/authenticate")
     public ResponseEntity<HttpStatus> login(@RequestBody LoginModel login) throws UsuarioHanding {
         if (!check(login)) {
             LoginModel loginFound = loginRepository.findByUsername(login.getUsername());
             if(loginFound.getPassword().equals(login.getPassword())) {
-                return new ResponseEntity<>(HttpStatus.OK);
+                return new ResponseEntity<>(HttpStatus.ACCEPTED);
             }
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }

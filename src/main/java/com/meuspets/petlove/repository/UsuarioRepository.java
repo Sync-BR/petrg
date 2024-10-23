0a9694af75c2eb016650f001176597e0da4a99c9
eduardo freitas <eduardofreitasfr@gmail.com>
2024-10-23 16:09:31 -0300

    Edited for new searches

--- src/main/java/com/meuspets/petlove/repository/UsuarioRepository.java
+++ src/main/java/com/meuspets/petlove/repository/UsuarioRepository.java
@@ -4,4 +4,5 @@ import com.meuspets.petlove.model.UsuarioModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
+    UsuarioModel findByEmail(String email);
 }

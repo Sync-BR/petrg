4976f70e4966ca0369ccb320c76f2c7791d2d235
eduardo freitas <eduardofreitasfr@gmail.com>
2024-10-23 16:08:51 -0300

    Add new feature

--- src/main/java/com/meuspets/petlove/controller/service/RegistrationData.java
+++ src/main/java/com/meuspets/petlove/controller/service/RegistrationData.java
@@ -1,4 +1,19 @@
 package com.meuspets.petlove.controller.service;
 
+import com.meuspets.petlove.model.UsuarioModel;
+import com.meuspets.petlove.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/validar/")
+@RestController
 public class RegistrationData {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public boolean validEmail(String email) {
+        UsuarioModel existingUser = usuarioRepository.findByEmail(email);
+        return existingUser != null;
+    }
 }

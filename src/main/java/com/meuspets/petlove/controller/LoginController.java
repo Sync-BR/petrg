21b4a1522a363f0ff36e2b595abcf53e24219596
eduardo freitas <eduardofreitasfr@gmail.com>
2024-10-24 20:52:37 -0300

    Added login check

--- src/main/java/com/meuspets/petlove/controller/LoginController.java
+++ src/main/java/com/meuspets/petlove/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.meuspets.petlove.controller;
 
+import com.meuspets.petlove.controller.service.RegistrationData;
 import com.meuspets.petlove.handling.UsuarioHanding;
 import com.meuspets.petlove.model.LoginModel;
 import com.meuspets.petlove.repository.LoginRepository;
@@ -14,7 +15,15 @@ import org.springframework.web.bind.annotation.*;
 public class LoginController extends UserService {
     @Autowired
     private LoginRepository loginRepository;
-
+    @Autowired
+    private RegistrationData registrationData;
+    @GetMapping("/check/username/{username}")
+    public ResponseEntity<Boolean> checkUserName(@PathVariable String username) {
+        if (registrationData.validUserName(username)) {
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(false, HttpStatus.OK);
+    }
     @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/authenticate")
     public ResponseEntity<HttpStatus> login(@RequestBody LoginModel login) throws UsuarioHanding {

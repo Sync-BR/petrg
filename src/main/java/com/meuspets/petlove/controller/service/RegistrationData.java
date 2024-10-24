66a0f994f3baf6bf7b85f3a40c7f6aab1588657b
eduardo freitas <eduardofreitasfr@gmail.com>
2024-10-24 20:53:16 -0300

    Add function to check user

--- src/main/java/com/meuspets/petlove/controller/service/RegistrationData.java
+++ src/main/java/com/meuspets/petlove/controller/service/RegistrationData.java
@@ -1,6 +1,8 @@
 package com.meuspets.petlove.controller.service;
 
+import com.meuspets.petlove.model.LoginModel;
 import com.meuspets.petlove.model.UsuarioModel;
+import com.meuspets.petlove.repository.LoginRepository;
 import com.meuspets.petlove.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegistrationData {
     @Autowired
     private UsuarioRepository usuarioRepository;
-
+    @Autowired
+    private LoginRepository loginRepository;
     public boolean validEmail(String email) {
         UsuarioModel existingUser = usuarioRepository.findByEmail(email);
         return existingUser != null;
     }
     public boolean validCpf(String cpf) {
         UsuarioModel existingUser = usuarioRepository.findByCpf(cpf);
+        return existingUser != null;
+    }
+    public boolean validUserName(String username) {
+        LoginModel existingUser = loginRepository.findByUsername(username);
         return existingUser != null;
     }
 }
